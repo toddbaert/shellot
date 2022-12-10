30e0b317ae39605c2cf1f67f7fcfb975525fbb59
Todd Baert <toddbaert@gmail.com>
2022-12-10 12:48:46 -0500

    wip: "And" support... smarter parsing
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- samples/sum-and-concat-test.sh
+++ samples/sum-and-concat-test.sh
@@ -2,11 +2,17 @@
 
 Given "\"([a-z]*)\" and \"([a-z]*)\" are appended" append_when
 Then "the result is \"([a-z]*)\"" append_then
+
 When "([0-9]+) and ([0-9]+) are added" sum_when
 Then "the result is ([0-9]+)" sum_then
 
+# When "([0-9]+) is multiplied by ([0-9]+)" multiply_when
+# And "the product is multiplied by ([0-9]+)" multiply_and
+# Then "the product ([0-9]+)" multiply_then
+
 declare concatenated
 declare sum
+declare product
 
 function append_when () {
   concatenated=$1$2
@@ -26,6 +32,22 @@ function sum_when () {
 
 function sum_then () {
   if [[ $1 -eq $sum ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function multiply_when () {
+  product=$(( $1 * $2 ))
+}
+
+function multiply_and () {
+  product=$(( $product * $1 ))
+}
+
+function multiply_then () {
+  if [[ $1 -eq $product ]]; then
     return 0
   else
     return 1
--- samples/sum-and-concat.feature
+++ samples/sum-and-concat.feature
@@ -7,3 +7,8 @@ Feature: Tests with injected values
   Scenario: Addition
     When 20 and 30 are added
     Then the result is 50
+
+  Scenario: Addition
+    When 1 is multiplied by 2 
+    And the product is multiplied by 4
+    Then the product is 8
